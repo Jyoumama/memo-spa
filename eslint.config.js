d3c7d7aa2a95257ad5addb3bdf9b4a48b26607a2
jyouma <juri19771211@gmail.com>
2024-12-30 09:07:44 +0900

    eslint.config.jsにReactの未使用関数に関しては無視する設定を追加

--- eslint.config.js
+++ eslint.config.js
@@ -25,7 +25,13 @@ export default [
       "react/react-in-jsx-scope": "off",
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": "warn",
-      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-unused-vars": [
+        "warn", 
+        { 
+          varsIgnorePattern: "React",
+          argsIgnorePattern: "^_",
+        }
+      ],
       "no-console": "warn",
     },
   },
